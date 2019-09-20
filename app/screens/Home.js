aad81fb211f2873b8fc00572db0c5c5aff8a4008
leoayresec <leoayresec@gmail.com>
2019-09-20 16:13:32 -0300

    Criando mapa

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,18 +1,46 @@
 import React, { PureComponent } from 'react';
-import {  View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps'
 
 export default class Home extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      marginTop: 1
     };
   }
-
   render() {
     return (
-      <View>
-        <Text> componentText </Text>
+      <View style={styles.container}>
+        <MapView style={[styles.map, { marginTop: this.state.marginTop }]}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+
+          }}
+          onMapReady={() => this.setState({ marginTop: 0 })}>
+        </MapView>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+
+  container: {
+
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+
+});
