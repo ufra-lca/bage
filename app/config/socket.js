baa53ecf4aaf986fdcab7ba5355299cb2fc34c91
AmadorZcv <lucasamadorzcv@gmail.com>
2019-09-20 17:15:58 -0300

    Conectando no socket

--- app/config/socket.js
+++ app/config/socket.js
@@ -1,13 +1,15 @@
 import { Socket } from 'phoenix'
 
 export function connectSocket() {
-    const ROOT_SOCKET = '10.11.81.17:4000';
+    const ROOT_SOCKET = 'http://10.11.81.17:4000/socket';
     socket = new Socket(`${ROOT_SOCKET}`, {});
+    socket.connect()
+
     let channel = socket.channel("room:lobby", {})
     channel.on("bage_update", (payload) => {
         console.log(payload)
     })
     channel.join()
-        .receive("Ok", resp => { console.log("Conectou", resp) })
-        .receive("Error", resp => { console.log("Não Conectou", resp) })
+        .receive("ok", resp => { console.log("Joined successfully", resp) })
+        .receive("error", resp => { console.log("Unable to join", resp) })
 }
