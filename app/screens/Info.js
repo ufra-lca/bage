25d728316345f2538550d74ef555d0811c1e2fab
AmadorZcv <lucasamadorzcv@gmail.com>
2019-09-21 14:21:05 -0300

    Adicionando logo do lab

--- app/screens/Info.js
+++ app/screens/Info.js
@@ -1,16 +1,22 @@
 import React, { PureComponent } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Text, Image } from "react-native-elements";
 
 export default class Info extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return (
-      <View>
-        <Text> componentText </Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View style={{ flex: 1 }}>
+          <Text>Horarios</Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("../img/logo2.jpeg")}
+            style={{ width: 360, height: 300 }}
+            resizeMode={"contain"}
+            resizeMethod={"resize"}
+          />
+        </View>
       </View>
     );
   }
